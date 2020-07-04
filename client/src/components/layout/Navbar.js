1c4b87ca10a97d9ada0095f332b480c7543f6cc3
Sam Vanderlinden <samvanderlinden6@gmail.com>
2020-07-04 11:56:35 -0500

    logout user

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,11 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
+import AuthContext from '../../context/auth/authContext';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
 const Navbar = () => {
+    const authContext = useContext(AuthContext);
+
+    const { isAuthenticated, logout } = authContext;
+
     useEffect(() => {
         let sidenav = document.querySelector('#slide-out');
         M.Sidenav.init(sidenav, {});
     });
+
+    const onLogout = () => {
+        logout();
+    }
     return (
         <>
             <nav>
@@ -14,14 +23,21 @@ const Navbar = () => {
                     <a href="#!" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
                     <ul className="right hide-on-med-and-down">
                         <li><a href="/register">Register</a></li>
-                        <li><a href="/login">Login</a></li>
+                        {isAuthenticated ? (
+                            <li><a href="/login" onClick={onLogout}>Logout</a></li>
+                        ) : (
+                                <li><a href="/login">Login</a></li>
+                            )}
                     </ul>
                 </div>
             </nav>
 
             <ul className="sidenav" id="slide-out">
-                <li><a href="/register">Register</a></li>
-                <li><a href="/login">Login</a></li>
+                <li><a href="/register">Register</a></li>{isAuthenticated ? (
+                    <li><a href="/login" onClick={onLogout}>Logout</a></li>
+                ) : (
+                        <li><a href="/login">Login</a></li>
+                    )}
             </ul>
         </>
     )
