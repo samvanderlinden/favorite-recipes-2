2d22ff51f5faed7e0beb574a96db69ce9ef79055
Sam Vanderlinden <samvanderlinden6@gmail.com>
2020-07-04 15:05:31 -0500

    add user's name to navbar when logged in

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,7 +5,7 @@ import M from 'materialize-css/dist/js/materialize.min.js';
 const Navbar = () => {
     const authContext = useContext(AuthContext);
 
-    const { isAuthenticated, logout } = authContext;
+    const { isAuthenticated, logout, user } = authContext;
 
     useEffect(() => {
         let sidenav = document.querySelector('#slide-out');
@@ -15,6 +15,7 @@ const Navbar = () => {
     const onLogout = () => {
         logout();
     }
+
     return (
         <>
             <nav>
@@ -22,6 +23,7 @@ const Navbar = () => {
                     <a href="#!" className="brand-logo">Recipe Logger</a>
                     <a href="#!" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
                     <ul className="right hide-on-med-and-down">
+                        {isAuthenticated && user && <li><strong>Welcome {user.name}</strong></li>}
                         <li><a href="/register">Register</a></li>
                         {isAuthenticated ? (
                             <li><a href="/login" onClick={onLogout}>Logout</a></li>
