8106760eebfddb07cee42a6ef2bca26f5da0e14c
Sam Vanderlinden <samvanderlinden6@gmail.com>
2020-07-06 09:58:31 -0500

    init AddRecipeButton on page load

--- client/src/components/layout/AddRecipeButton.js
+++ client/src/components/layout/AddRecipeButton.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
 const AddRecipeButton = () => {
+    useEffect(() => {
+        let addRecipeModal = document.querySelector('#add-recipe-modal');
+        M.Modal.init(addRecipeModal, {});
+    });
     return (
         <div className="fixed-action-btn">
             <a href="#add-recipe-modal" className="btn-floating btn-large blue modal-trigger">
