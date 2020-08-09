3b8bde17afb07d8537585c58593cc93e244a961f
Sam Vanderlinden <samvanderlinden6@gmail.com>
2020-08-09 07:43:14 -0500

    prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-// const path = require('path');
+const path = require('path');
 
 const app = express();
 
@@ -15,7 +15,13 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/recipes', require('./routes/recipes'));
 
-app.get('/', (req, res) => res.json({message: 'Welcome to the Favorite Recipes API!!'}));
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
